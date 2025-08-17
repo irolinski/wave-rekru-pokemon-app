cf08a101f6b502260a4050fc212e6c727b1ffcde
irolinski <ignacy.rolinski@gmail.com>
2025-08-17 17:36:21 +0200

    feat: show pokeball when sprite isLoading

---
+++ assets/index.ts
@@ -0,0 +1,3 @@
+import { pokeBall } from './poke-ball.png';
+
+export { pokeBall };
--- components/PokemonCard.tsx
+++ components/PokemonCard.tsx
@@ -1,10 +1,17 @@
 import { LinearGradient } from 'expo-linear-gradient';
-import { ColorValue, Image, Text, View } from 'react-native';
+import { router } from 'expo-router';
+import { useState } from 'react';
+import { ColorValue, Image, Text, TouchableOpacity, View } from 'react-native';
+import { pokeBall } from '~/assets';
 import { typeColors } from '~/constants/styles/colors';
 import { PokemonCardData, PokemonTypeProperty } from '~/types';
 import TypePill from './TypePill';
 
+// Import your local asset
+
 const PokemonCard = ({ data }: { data: PokemonCardData }) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
   // Determine background colors
   const bgColors: ColorValue[] | ColorValue =
     data.types.length === 1
@@ -12,7 +19,9 @@ const PokemonCard = ({ data }: { data: PokemonCardData }) => {
       : data.types.map((type) => typeColors[type]); // gradient
 
   return (
-    <View className="relative my-2 h-44 w-44 flex-col items-center justify-center overflow-hidden rounded-2xl">
+    <TouchableOpacity
+      className="relative my-2 h-44 w-44 flex-col items-center justify-center overflow-hidden rounded-2xl"
+      onPress={() => router.push('/modal')}>
       <LinearGradient
         colors={bgColors}
         start={{ x: 0, y: 1 }}
@@ -20,8 +29,13 @@ const PokemonCard = ({ data }: { data: PokemonCardData }) => {
         style={{ width: '100%', height: '100%' }}
         className="absolute">
         <View className="w-full flex-row justify-center">
-          <Image source={{ uri: data.sprite }} className="h-20 w-20" />
+          <Image
+            source={imageLoaded ? { uri: data.sprite } : pokeBall}
+            className="h-20 w-20"
+            onLoadEnd={() => setImageLoaded(true)}
+          />
         </View>
+
         <View className="mt-2 flex-col items-center">
           <Text className="text-lg font-bold capitalize text-white">{data.name}</Text>
           <View className="mt-1 flex-row items-center justify-center">
@@ -31,7 +45,7 @@ const PokemonCard = ({ data }: { data: PokemonCardData }) => {
           </View>
         </View>
       </LinearGradient>
-    </View>
+    </TouchableOpacity>
   );
 };
 
