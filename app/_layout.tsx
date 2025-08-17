245c32910d608cf58cd81c4e2d95beb00b7907b1
irolinski <ignacy.rolinski@gmail.com>
2025-08-17 17:33:08 +0200

    fix: show status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,7 @@ import { StatusBar } from 'expo-status-bar';
 export default function RootLayout() {
   return (
     <>
-      <StatusBar hidden />
+      <StatusBar />
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="index" />
         <Stack.Screen
